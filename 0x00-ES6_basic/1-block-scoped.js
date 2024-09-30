cae29c8cd1f835206cac0de49d65444b77a1af8f
@a_idk <idoko_research@outlook.com>
2024-09-30 22:08:24 +0000

    var to const

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlock(trueOrFalse) {
+  /* Change the "var" to "const" */
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
+}
