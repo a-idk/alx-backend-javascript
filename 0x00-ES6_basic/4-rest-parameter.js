c04837cd32f841a3af236a5d8e788f17904d8bbc
@a_idk <idoko_research@outlook.com>
2024-09-30 22:35:54 +0000

    rest parameter syntax

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,2 +1,5 @@
-const returnHowManyArguments = (...args) => args.length;
-export default returnHowManyArguments;
+/* const returnHowManyArguments = (...args) => args.length;
+export default returnHowManyArguments; */
+export default function returnHowManyArguments(...args) {
+  return args.length;
+}
