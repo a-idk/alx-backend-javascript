a55d75928ed2af030cb3e884f041d6c130c7ff18
@a_idk <idoko_research@outlook.com>
2024-10-07 15:59:36 +0000

    task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // setting amount
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  // getting amount
+  get amount() {
+    return this._amount;
+  }
+
+  // setting currency
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  // getting currency
+  get currency() {
+    return this._currency;
+  }
+
+  // displaying the price
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  // Converting to price 
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
